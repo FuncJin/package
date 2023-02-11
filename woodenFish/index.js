db4872a31440223bc29650d90dc56a8fd8cecf9e
Ssylqwj1212 <1767439745@qq.com>
2023-02-11 17:07:59 +0800

    feat: Tap the H5 of wood fish

---
+++ woodenFish/index.js
@@ -0,0 +1,51 @@
+const dom = {
+    woodenFish: document.querySelector("img"),
+    text: document.querySelector(".w-f-c-text"),
+    audio: document.querySelector("audio")
+}
+// 控制木鱼大小、弹出文字等样式
+const woodenFish = {
+    className(type) {
+        dom.woodenFish.classList[type]("w-f-c-i-size")
+    },
+    size() {
+        this.className("add")
+        setTimeout(() => this.className("remove"), 300)
+    },
+    createText: () => {
+        const p = document.createElement("p")
+        p.innerText = "功德+1"
+        return p
+    }
+}
+// 振动
+const vibrate = () => {
+    const navigator = window.navigator
+    if (!("vibrate" in navigator)) return
+    navigator.vibrate =
+        navigator.vibrate ||
+        navigator.webkitVibrate ||
+        navigator.mozVibrate ||
+        navigator.msVibrate
+    if (!navigator.vibrate) return
+    navigator.vibrate(300)
+}
+// 在全屏或非全屏之间切换
+const toggleFullScreen = () => {
+    if (!document.fullscreenElement)
+        return document.documentElement.requestFullscreen()
+    if (!document.exitFullscreen) return
+    document.exitFullscreen()
+}
+
+dom.woodenFish.addEventListener("click", () => {
+    woodenFish.size()
+    dom.text.appendChild(woodenFish.createText())
+    dom.audio.play()
+    vibrate()
+})
+document.addEventListener("keydown", (e) =>
+    e.keyCode == 13 ? toggleFullScreen() : false
+)
+
+console.log("\n按下Enter键，可以在全屏或非全屏之间来回切换。\n\n")
